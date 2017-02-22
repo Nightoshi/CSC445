bd128938bb9640eb41c46ba592e79e14f935ef88
Lisa <lye@missouriwestern.edu>
2017-02-22 15:21:50 -0600

    Next Project

---
+++ CrminalIntent/app/src/main/java/csc445/lye/edu/missouriwestern/criminalIntent/CrimeActivity.java
@@ -0,0 +1,13 @@
+package csc445.lye.edu.missouriwestern.criminalIntent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CrimeActivity extends FragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crime);
+    }
+}
