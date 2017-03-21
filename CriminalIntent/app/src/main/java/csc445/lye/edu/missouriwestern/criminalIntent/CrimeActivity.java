994a78789a9c87186c0093e4d36cc865c99c1a27
lyeMWSU <lye@missouriwestern.edu>
2017-03-21 16:42:19 -0500

    update

--- CriminalIntent/app/src/main/java/csc445/lye/edu/missouriwestern/criminalIntent/CrimeActivity.java
+++ CriminalIntent/app/src/main/java/csc445/lye/edu/missouriwestern/criminalIntent/CrimeActivity.java
@@ -1,22 +1,14 @@
-<<<<<<< e76e72ed08f722e9079f21f6d5c77b5fa8da2ea9
+
 package csc445.lye.edu.missouriwestern.criminalIntent;
 
 import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class CrimeActivity extends FragmentActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_crime);
-    }
-=======
 public class CrimeActivity extends SingleFragmentActivity {
+
 	@Override
 	protected Fragment createFragment(){
-		return new CrimeFragment();
+	return new CrimeFragment();
 	}
->>>>>>> Criminal Intent
 }
